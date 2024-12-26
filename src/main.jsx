52aa2f8f4eff2c54a5d1074dbc37951687ca373e
Vlad Iliev <orionivvlad@gmail.com>
2024-12-26 15:28:04 +0200

    Update search input value, refactor time filter component, and optimize dependencies.

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDom from 'react-dom/client'
 import App from './App.jsx'
 import 'bootstrap/dist/css/bootstrap.min.css'
-import './components/TimeFrame/TimeFrame.css'
+import './components/TimeFilter/TimeFilter.css'
 import './components/WeatherContainer/WeatherContainer.css'
 import 'rsuite/dist/rsuite-no-reset.min.css'
 
